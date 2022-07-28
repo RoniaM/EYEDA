82bb7f9524c0fa714ad44dd7a9bc85481aa67fe3
Nolan <77746688+Sha-dos@users.noreply.github.com>
2022-07-27 23:18:13 -0500

    added grayscale

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,19 @@ fn main() -> Result<()> {
     if !opened { panic!("Unable to open default camera!"); }
     loop {
         let mut frame = Mat::default();
+
         cam.read(&mut frame)?;
+
         if frame.size()?.width > 0 {
-            highgui::imshow("frame", &frame)?;
+            let mut gray = Mat::default();
+            imgproc::cvt_color(
+                &frame,
+                &mut gray,
+                imgproc::COLOR_BGR2GRAY,
+                0,
+            )?;
+
+            highgui::imshow("frame", &gray)?;
         }
         if highgui::wait_key(10)? > 0 {
             break;
